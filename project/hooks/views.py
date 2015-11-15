4e95250c5b99b5e89109d726137936544fea39bf
Pete Wildsmith <pete@weargoggles.co.uk>
2015-11-15 20:04:45 +0000

    try receiving and authenticating webhooks from github

---
+++ project/hooks/views.py
@@ -0,0 +1,26 @@
+import binascii
+from cryptography.hazmat.primitives import hmac
+from cryptography.hazmat.primitives.hashes import SHA1
+from cryptography.hazmat.primitives.constant_time import bytes_eq
+import cryptography.hazmat.backends
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import HttpResponse
+
+crypto_backend = cryptography.hazmat.backends.default_backend()
+
+
+def verify_signature(request, request_body):
+    hmac_hmac = hmac.HMAC(settings.GITHUB_WEBHOOK_SECRET, SHA1, crypto_backend)
+    hmac_hmac.update(request_body)
+    signature = b'sha1=' + binascii.hexlify(hmac_hmac.finalize())
+    return bytes_eq(signature, request.META['HTTP_X_HUB_SIGNATURE'])
+
+
+def receive_hook(request):
+    verify_signature(request, request.body)
+    send_mail('Hook from github', request.body, settings.SERVER_EMAIL,
+              map(lambda t: t[-1], settings.ADMINS))
+    return HttpResponse(status=200)
+
+
--- project/urls.py
+++ project/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 import project.test.views as test_views
 import project.home.views as home
 import project.login.views as login
+import project.hooks.views as hooks
 
 urlpatterns = [
     url(r'^test/$', test_views.test),
     url(r'^$', home.home, name='home'),
     url(r'^login/$', login.login, name='login'),
     url(r'^login/callback/$', login.login_callback, name='login-callback'),
+    url(r'^hook/$', hooks.receive_hook, name='receive-hook'),
 ]
