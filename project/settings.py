35f4b0ca57716d882995a446ac3946389f7e95a2
Pete Wildsmith <pete@weargoggles.co.uk>
2015-11-15 13:23:10 +0000

    explicit import from PyCrypto

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import Crypto
+import Crypto.PublicKey.RSA
 
 import os
 import environ
