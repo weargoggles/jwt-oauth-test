ace8b88d70534cbc693dd0cc4ac7175d597f460c
Pete Wildsmith <pete@weargoggles.co.uk>
2015-11-15 19:29:55 +0000

    list repos on homepage if logged in

--- project/home/views.py
+++ project/home/views.py
@@ -1,6 +1,17 @@
+import requests
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
 def home(request):
-    return render_to_response("home.html", RequestContext(request))
+    repos_response = requests.get('https://api.github.com/v3/user/repos', headers={
+        'Authorization': 'token %s' % request.session['github']['access_token'],
+        'Accept': 'application/json',
+    })
+    if repos_response.status_code == 200:
+        repos = repos_response.json()
+    else:
+        repos = []
+    return render_to_response("home.html", {
+        'repos': repos,
+    }, RequestContext(request))
