f97c289d4ed811a2dc50ef47c499a91a00710c87
Pete Wildsmith <pete@weargoggles.co.uk>
2015-11-15 14:44:47 +0000

    form for updating session hohohoh

--- project/test/views.py
+++ project/test/views.py
@@ -1,6 +1,11 @@
+from django.core.serializers import json
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 def test(request):
     request.session['test'] = True
-    return HttpResponse('test')
+    if request.method == "POST":
+        request.session['posted'] = request.POST['posted']
+    return render_to_response('test.html', RequestContext(request))
